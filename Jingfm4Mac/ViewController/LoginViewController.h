19b19ebb860b2f73ea892ebaaafc4afedede76e3
Luyiyuan <luyiyuan@umeng.com>
2012-11-17 18:07:21 +0800

    添加login界面
    Signed-off-by: Luyiyuan <luyiyuan@umeng.com>

---
+++ Jingfm4Mac/ViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Jingfm4Mac
+//
+//  Created by luyiyuan on 11/17/12.
+//  Copyright (c) 2012 tapray.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LoginViewController : NSViewController
+
+@end
---
+++ Jingfm4Mac/ViewController/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.m
+//  Jingfm4Mac
+//
+//  Created by luyiyuan on 11/17/12.
+//  Copyright (c) 2012 tapray.com. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
