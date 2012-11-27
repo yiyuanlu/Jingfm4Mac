353f86f2f1ebd56515d438556aa3b4c2e5c53f75
Luyiyuan <luyiyuan@umeng.com>
2012-11-27 14:17:58 +0800

    添加播放按钮
    Signed-off-by: Luyiyuan <luyiyuan@umeng.com>

---
+++ Jingfm4Mac/ViewController/PlayingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayingViewController.h
+//  Jingfm4Mac
+//
+//  Created by luyiyuan on 11/27/12.
+//  Copyright (c) 2012 tapray.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlayingViewController : NSViewController
+
+@end
---
+++ Jingfm4Mac/ViewController/PlayingViewController.m
@@ -0,0 +1,27 @@
+//
+//  PlayingViewController.m
+//  Jingfm4Mac
+//
+//  Created by luyiyuan on 11/27/12.
+//  Copyright (c) 2012 tapray.com. All rights reserved.
+//
+
+#import "PlayingViewController.h"
+
+@interface PlayingViewController ()
+
+@end
+
+@implementation PlayingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
